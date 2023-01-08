808dc3a5277bf66f1c3a1f767822bb5bae3159f9
Piotr Łasek <piotrek10.lasek@o2.pl>
2023-01-08 11:05:37 +0100

    GBH-16 Przygotowanie frontendu do pracy     * Utworzono poprawną strukture projektu

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './core/main/main.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent, children: [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomePageComponent}
+  ]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
