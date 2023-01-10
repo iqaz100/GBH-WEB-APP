719109f8f3113e51b55eaebb0b71a58670643770
Grzegorz Górka <grzegorz.gorka@microsoft.wsei.edu.pl>
2023-01-10 15:56:17 +0100

    Added components for login, register and reminder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MainComponent } from './core/main/main.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LoginComponent } from './features/login-fields/login/login.component';
+import { RegisterComponent } from './features/login-fields/register/register.component';
+import { ReminderComponent } from './features/login-fields/reminder/reminder.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    MainComponent
+    MainComponent,
+    LoginPageComponent,
+    LoginComponent,
+    RegisterComponent,
+    ReminderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
