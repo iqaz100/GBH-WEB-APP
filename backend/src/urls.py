6a4bbf30a2d58d608f5a7062c65b9a9fb9469136
iqaz100 <piotrek10.lasek@o2.pl>
2023-02-25 22:09:17 +0100

    GBH-13 Utworzenie endpointów umożliwiajacych zalogowanie do aplikacji * Dodano endpointy logowania i rejestracji

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from rest_framework import permissions
-# from rest_framework.schemas import get_schema_view
 from drf_yasg.views import get_schema_view
 
 from api import router
@@ -34,6 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('apps.auth.urls')),
     path("api/", include(router.urls)),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
